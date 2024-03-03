4253ff2a367c16e27f53e74efd74a9f7c3ba8fcf
Aleksandr13579 <al13579qw@yandex.ru>
2024-03-04 01:22:34 +0300

    change configure db

--- ddl_constructor.sql
+++ ddl_constructor.sql
@@ -1,7 +1,5 @@
-CREATE DATABASE "parsing_site";
-\connect "parsing_site";
-CREATE ROLE vagrant WITH PASSWORD 'vagrant' NOSUPERUSER, LOGIN;
-CREATE SCHEMA "parse_site";
-CREATE TABLE IF NOT EXISTS "parser_site.parsed_data" (title VARCHAR(255), description TEXT);
-ALTER SCHEMA "parse_site" OWNER TO 'vagrant';
-ALTER TABLE "parser_site.parsed_data" OWNER TO 'vagrant';
+CREATE ROLE vagrant WITH PASSWORD 'vagrant' NOSUPERUSER LOGIN;
+CREATE SCHEMA parser_site
+	CREATE TABLE IF NOT EXISTS parsed_data (title VARCHAR(255), description TEXT);
+ALTER SCHEMA parser_site OWNER TO vagrant;
+ALTER TABLE parser_site.parsed_data OWNER TO vagrant;
